f5c17e46611017d8ea27dc40b860cf9d430d9fc4
DragonQrbx <mevlutsaglam153@gmail.com>
2026-05-21 12:41:19 +0300

    feat: add initial problematic shopping cart implementation

---
+++ src/model/ShoppingCart.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+
+    ArrayList<String> productNames = new ArrayList<>();
+    ArrayList<Double> productPrices = new ArrayList<>();
+
+    public void addProduct(String name, double price) {
+
+        productNames.add(name);
+        productPrices.add(price);
+
+        System.out.println(name + " sepete eklendi");
+    }
+
+    public void calculateTotal(String discountType) {
+
+        double total = 0;
+
+        for (double price : productPrices) {
+            total += price;
+        }
+
+        if (discountType.equals("ogrenci")) {
+
+            total = total * 0.50;
+            System.out.println("ogrenci indirimi uygulandi ");
+
+        } else if (discountType.equals("engelli")) {
+
+            total = total * 0.99;
+            System.out.println("engelli indirimi uygulandı.");
+
+        }  else {
+
+            System.out.println("gecersiz islem");
+        }
+
+        System.out.println("toplam ucret: " + total);
+    }
+}
